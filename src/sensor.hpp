18858349a14f4ff468c51e6a1a96bb74b05c913b
Simone Zama <simone.zama@studio.unibo.it>
2022-11-14 11:46:55 +0100

    Implement sensor interface

---
+++ src/sensor.hpp
@@ -0,0 +1,17 @@
+#ifndef __SENSOR_H__
+#define __SENSOR_H__
+
+/**
+ * @brief Class that defines a generic sensor 
+ * 
+ */
+class Sensor
+{
+private:
+protected:
+    Sensor();
+public:
+    ~Sensor();
+};
+
+#endif
