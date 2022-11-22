13bcf53f6034fa4167e46bb0ffc16eafbd98ca70
Angela Speranza <angela.speranza@studio.unibo.it>
2022-11-22 09:40:09 +0100

    add python serial comm code

--- SmartBridge/src/main.cpp
+++ SmartBridge/src/main.cpp
@@ -4,11 +4,15 @@
 bool debug = false;
 
 Status currentStatus = NORMAL;
+int x;
 
 void setup() {
-    // put your setup code here, to run once:
+    Serial.begin(9600);
+  Serial.setTimeout(1);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    while (!Serial.available());
+    x = Serial.readString().toInt();
+    Serial.print(x + 1);
 }
