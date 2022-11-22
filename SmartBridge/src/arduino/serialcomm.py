f2c2973e7205738bf2454beebaa51a9b1e46e0a1
Angela Speranza <angela.speranza@studio.unibo.it>
2022-11-22 11:03:04 +0100

    add code for serial ports

--- SmartBridge/src/arduino/serialcomm.py
+++ SmartBridge/src/arduino/serialcomm.py
@@ -1,8 +1,16 @@
 # Importing Libraries
 import serial
 import time
+import sys
+import serial.tools.list_ports
 
-arduino = serial.Serial('COM5', 9600, timeout=.1) #Creating our serial object named arduino
+ports = serial.tools.list_ports.comports()
+
+for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
+
+
+"""arduino = serial.Serial('COM5', 9600, timeout=.1)
 
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
@@ -12,4 +20,4 @@ def write_read(x):
 while True:
     num = input("Enter a number: ") # Taking input from user
     value = write_read(num)
-    print(value) # printing the value
+    print(value) # printing the value """
