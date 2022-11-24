3af4236c572a0c99cc6fd0abe1f177d17b6f7e10
Angela Speranza <angela.speranza@studio.unibo.it>
2022-11-24 11:01:59 +0100

    add macros

--- SmartBridge/src/arduino/Config.h
+++ SmartBridge/src/arduino/Config.h
@@ -1,6 +1,9 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+#define WL_MAX 3.0  // Threshold for ALARM
+#define WL_2 5.0    // Threshold for PRE_ALARM
+
 enum Status {
     NORMAL, 
     PRE_ALARM,
